cfaa48e003f9ab92e583390555abc874f8622d98
Giovane Matos <filho1987@gmail.com>
2022-09-13 16:13:21 -0300

    Tema

--- mobile/App.js
+++ mobile/App.js
@@ -1,14 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+
+interface ButtonProps{
+  title: String;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>  
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <Button title="Send 1" />
+      <Button title="Send 2" />
+      <Button title="Send 3" />
+      <Button title="Hello World" />
+
       <StatusBar style="auto" />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +38,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
 });
