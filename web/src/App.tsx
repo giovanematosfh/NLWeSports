35671c053f3395359091def0f60cef1b23d8b6dd
Giovane Matos <filho1987@gmail.com>
2022-09-16 10:29:04 -0300

    Finalizando css landpage

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,4 @@
+import {MagnifyingGlassPlus} from 'phosphor-react'
 import './styles/main.css'
 
 import logotipo from './assets/logo-nlw-esports.svg';
@@ -68,7 +69,8 @@ function App() {
             <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
           </div>
 
-          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded">
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+            <MagnifyingGlassPlus size={24} />
             Publicar Anuncio
           </button>
 
