ea7f87c5dee4b9246c0415228fffce00abe29c59
Giovane Matos <filho1987@gmail.com>
2022-09-18 21:35:05 -0300

    teste

---
+++ server/src/README.md
@@ -0,0 +1,10 @@
+Instalando o prisma em modo desenvolvimento:
+npm i prisma -D 
+
+EXECULTAR O PRISMA
+npx prisma init
+
+npx prisma init --datasource-provider SQLite
+
+Rodar a migrate
+npx prisma migrate dev
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,19 @@
-import express, { response }  from "express";
+import express, { request, response }  from "express";
 
 const app = express()
 
 // localhost:3333/ads
 
-app.get('/ads', (request, response) => {
+app.get('/games', (request, response) => {
+   return response.json([]);
+});
+
+app.post('/ads', (request, response) => {
+    return response.status(201).json([]);
+});
+
+
+app.get('/games/:id/ads', (request, response) => {
     return response.json([
         {id:1, name:'Anuncio 1'},
         {id:2, name:'Anuncio 2'},
